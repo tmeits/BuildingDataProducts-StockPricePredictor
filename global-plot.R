43ce0586dc9982217b25575ca54349a09f062a5d
tmeits <tmeits@gmail.com>
2017-04-27 15:34:47 +0700

    Add files via upload

---
+++ global-plot.R
@@ -0,0 +1,23 @@
+#
+library(plotly)
+
+plotlyMarkersLines <- function(){
+  trace_0 <- rnorm(100, mean = 5)
+  trace_1 <- rnorm(100, mean = 0)
+  trace_2 <- rnorm(100, mean = -5)
+  x <- c(1:100)
+  
+  data <- data.frame(x, trace_0, trace_1, trace_2)
+  
+  p <- plot_ly(data, x = ~x, y = ~trace_0, name = 'trace 0', type = 'scatter', mode = 'lines') %>%
+    add_trace(y = ~trace_1, name = 'trace 1', mode = 'lines+markers') %>%
+    add_trace(y = ~trace_2, name = 'trace 2', mode = 'markers') %>%
+    layout(title = "title",
+             xaxis = list(title = "Days"), 
+             yaxis = list(title = "Temp"))
+  p
+}
+
+if (FALSE){
+  plotlyMarkersLines()
+}
